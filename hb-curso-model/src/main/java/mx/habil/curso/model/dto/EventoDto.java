fcc568b5882e0a73224a56babf0bea3ef58a1b85
Dorian Chávez Juárez <dchavez@HB-DCJ.local>
2015-02-07 00:49:50 -0600

    ajustes al modelo

---
+++ hb-curso-model/src/main/java/mx/habil/curso/model/dto/EventoDto.java
@@ -0,0 +1,54 @@
+package mx.habil.curso.model.dto;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+import mx.habil.support.HabilDto;
+
+
+@Entity
+@Table(name="HB_EVENTO")
+public class EventoDto extends HabilDto{
+	private static final long serialVersionUID = 20150207001L;
+	
+	@Id
+	@GeneratedValue(generator="uuid")
+	@GenericGenerator(name="uuid", strategy="uuid2")
+	@Column(name="ID_EVT", unique=true, nullable=false, length=37)
+	private @Getter @Setter String id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="FECEVT", nullable=false)
+	private @Getter @Setter Date fechaEvento;
+	
+	@Column(name="EVT", length=80, nullable=false)
+	private @Getter @Setter String evento;
+	
+	@Column(name="CLASE", length=80, nullable=false)
+	private @Getter @Setter String clase;
+	
+	@Column(name="SUBCLASE", length=80, nullable=false)
+	private @Getter @Setter String subClase;
+	
+	@Lob @Basic(fetch=FetchType.LAZY)
+	@Column(name="COMENTARIOS", nullable=true)
+	private @Getter @Setter String comentarios;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="FECEVT", nullable=false)
+	private @Getter @Setter Date fechaRegistro;
+}
