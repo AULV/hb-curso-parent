dbe4cdefcc2bd4bb14bc466a6d19185042db0266
Dorian Chávez Juárez <dorian.chavez@habilgroup.com>
2015-03-01 01:23:12 -0600

    Configuración Spring MVC

--- hb-curso/src/main/java/mx/habil/curso/front/vo/UsuarioVo.java
+++ hb-curso/src/main/java/mx/habil/curso/front/vo/UsuarioVo.java
@@ -1,13 +1,24 @@
 package mx.habil.curso.front.vo;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 
 public class UsuarioVo {
+	@NotNull
 	private @Getter @Setter String cveUsuario;
+	
+	@NotNull
 	private @Getter @Setter String nombre;
+	
+	@NotNull
 	private @Getter @Setter String apPaterno;
+	
+	@NotNull
 	private @Getter @Setter String apMaterno;
+	
+	@NotNull
 	private @Getter @Setter Boolean activo;
 	
 }	
